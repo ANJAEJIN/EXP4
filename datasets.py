c70c70816601416dd6eaffd17639e43063dc3014
AN JAEJIN <e175731@ie.u-ryukyu.ac.jp>
2019-11-05 16:23:17 +0900

    regression ver.2

--- datasets.py
+++ datasets.py
@@ -5,6 +5,21 @@ def load_linear_example1():
     >>> X, Y = load_linear_example1()
     >>> print(X[0])
     [1 4]
+
+    # testing (cont.)
+    >>> import regression
+    >>> model = regression.LinearRegression()
+    >>> print(model.x)
+    None
+
+    # testing (cont.)
+    >>> import importlib
+    >>> importlib.reload(regression)
+    <module 'regression' from '/Users/jaejinan/PycharmProjects/exp4/regression.py'>
+    >>> model = regression.LinearRegression()
+    >>> model.fit(X, Y)
+    >>> model.theta
+    array([5.30412371, 0.49484536])
     """
 
     X = np.array([[1,4],[1,8],[1,13],[1,17]])
--- regression.py
+++ regression.py
@@ -1,18 +1,13 @@
 import numpy as np
 class LinearRegression:
-    """
-    # testing (cont.)
-    >>> import regression
-    >>> model = regression.LinearRegression()
-    >>> print(model.x)
-    None
-    """
     x = None
     theta = None
     y = None
 
     def fit(self, x, y):
-        pass
+
+        temp = np.linalg.inv(np.dot(x.T, x))
+        self.theta = np.dot(np.dot(temp, x.T), y)
 
     def predict(self, x):
         pass

326dab0e93cad2a5ec147ee92f86147d58a04c05
AN JAEJIN <e175731@ie.u-ryukyu.ac.jp>
2019-11-05 16:17:41 +0900

    regression ver.1

---
+++ regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+class LinearRegression:
+    """
+    # testing (cont.)
+    >>> import regression
+    >>> model = regression.LinearRegression()
+    >>> print(model.x)
+    None
+    """
+    x = None
+    theta = None
+    y = None
+
+    def fit(self, x, y):
+        pass
+
+    def predict(self, x):
+        pass
+
+    def score(self, x, y):
+        pass

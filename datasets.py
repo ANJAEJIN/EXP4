2ede4ec5fc61931c65220868db00debd8320558b
AN JAEJIN <e175731@ie.u-ryukyu.ac.jp>
2019-11-12 15:41:33 +0900

    datasets ver.3

--- datasets.py
+++ datasets.py
@@ -64,3 +64,21 @@ def polynomial2_features(input):
     """
     poly2 = input[:,1:]**2
     return np.c_[input, poly2]
+
+def polynomial3_features(input):
+    """
+    # testing
+    >>> import datasets
+    >>> X, Y = datasets.load_nonlinear_example1()
+    >>> ex_X = datasets.polynomial3_features(X)
+    >>> ex_X
+    array([[ 1.   ,  0.   ,  0.   ,  0.   ],
+           [ 1.   ,  2.   ,  4.   ,  8.   ],
+           [ 1.   ,  3.9  , 15.21 , 59.319],
+           [ 1.   ,  4.   , 16.   , 64.   ]])
+    >>> Y
+    array([4., 0., 3., 2.])
+    """
+    poly2 = input[:,1:]**2
+    poly3 = input[:,1:]**3
+    return np.c_[input, poly2, poly3]

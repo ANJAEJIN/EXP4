fbb5fb48bdcee9511dc1477e452358d8bbaa9ee6
AN JAEJIN <e175731@ie.u-ryukyu.ac.jp>
2019-11-12 15:37:00 +0900

    datasets ver.2

--- datasets.py
+++ datasets.py
@@ -43,3 +43,24 @@ def load_linear_example1():
     Y = np.array([7,10,11,14])
 
     return X,Y
+
+def load_nonlinear_example1():
+    X = np.array([[1, 0.0], [1, 2.0], [1, 3.9], [1, 4.0]])
+    Y = np.array([4.0, 0.0, 3.0, 2.0])
+    return X, Y
+def polynomial2_features(input):
+    """
+    # testing
+    >>> import datasets
+    >>> X, Y = datasets.load_nonlinear_example1()
+    >>> ex_X = datasets.polynomial2_features(X)
+    >>> ex_X
+    array([[ 1.  ,  0.  ,  0.  ],
+           [ 1.  ,  2.  ,  4.  ],
+           [ 1.  ,  3.9 , 15.21],
+           [ 1.  ,  4.  , 16.  ]])
+    >>> Y
+    array([4., 0., 3., 2.])
+    """
+    poly2 = input[:,1:]**2
+    return np.c_[input, poly2]
